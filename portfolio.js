af272c096fe56260fece8d65dbf60cc22042815f
Fran <125502023+FranaGan5@users.noreply.github.com>
2023-08-23 17:09:08 +0200

    Delete portfolio.js

--- portfolio.js
+++
@@ -1,6 +0,0 @@
-let medio = prompt('Escoge un medio de transporte: '+' tren, autobus, coche, bicicleta');
-if( medio === 'tren'|| medio=== 'autobus'){
-    alert('Recuerda llevarte el dinero para el ticket')
-}else{
-    alert('El coche contamina, mejor haz uso de la bicicleta o transporte público')
-}
