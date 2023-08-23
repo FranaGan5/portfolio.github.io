46dba768f6e585b8eaa1f7e409171ac05b6d1e3f
Fran <125502023+FranaGan5@users.noreply.github.com>
2023-08-23 17:02:54 +0200

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,6 @@
+let medio = prompt('Escoge un medio de transporte: '+' tren, autobus, coche, bicicleta');
+if( medio === 'tren'|| medio=== 'autobus'){
+    alert('Recuerda llevarte el dinero para el ticket')
+}else{
+    alert('El coche contamina, mejor haz uso de la bicicleta o transporte público')
+}
---
+++ scripts/main.js
@@ -0,0 +1,1 @@
+alert('¡Hola! bienvenid@ a mi portfolio personal')
